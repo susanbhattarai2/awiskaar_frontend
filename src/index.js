68c9e629d35cf279651dbeb1e658432f245cf57a
SusanBhattarai <susanbhattrai36@gmail.com>
2022-12-01 14:44:24 +0545

    [Error] font

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import  {createRoot} from "react-dom/client";
 import App from "./App";
 import './index.css';
+import './font/Devant-Medium.ttf'
 createRoot(document.getElementById('root')).render(<App />);
