0bb12c1874de0b899cf202e64d8bd2eac6dfa0f2
SusanBhattarai <susanbhattrai36@gmail.com>
2022-12-22 23:10:56 +0545

    added typist

--- src/landingPage/banner/Banner.jsx
+++ src/landingPage/banner/Banner.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './banner.css'
 import ai from '../../assets/aiii.png';
+import Typist from 'react-text-typist';
 const Banner = () => (
   
   <div className="ap__banner section__padding scale-up-center" id="home">
     <div className="ap__banner-content">
       <h1 className='gradient__text'>Let&apos;s Build Something Amazing with Awiskaar </h1>
       {/* <p>Awiskaar Solutions is one of the fast emerging company in the valley. We provides every possible services related to the Technology field.</p> */}
-
-      <div className="ap__banner-content-button">
+      <p>We Belive in : <Typist className={'myTypist'} cursorClassName={'myCursor'} sentences={['First Sentence', 'Second Sentence', 'Third Sentence']} loop />
+       </p><div className="ap__banner-content-button">
             <button type="button"> <a href='#contact'>Get Started</a></button>
       </div>
     </div>
