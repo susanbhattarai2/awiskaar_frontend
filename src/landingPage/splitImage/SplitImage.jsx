876cd17b13a7b6fe5587059ccf57478e99df376e
SusanBhattarai <susanbhattrai36@gmail.com>
2022-12-22 23:26:58 +0545

    added spilt image

--- src/landingPage/splitImage/SplitImage.jsx
+++ src/landingPage/splitImage/SplitImage.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import './splitimage.css'
 const SplitImage = () => {
   return (
-    <>
-    <div className='hero'>
-      <div className="one">
-      </div>
-      <div className='two'>
-        two
-      </div>
-    </div>
-    </>
+    <div></div>
+    // <>
+    // <div className='hero'>
+    //   <div className="one">
+      
+      
+    //   </div>
+    //   <div className='three'>
+    //   <h1 className='gradient__text'>Let&apos;s Build Something Amazing with Awiskaar </h1>
+    //   <button type="button"> <a href='#contact'>Get Started</a></button>
+    //   </div>
+    //   <div className='two'>
+     
+    //   </div>
+    // </div>
+    // </>
   )
 }
 
