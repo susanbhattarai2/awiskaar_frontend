c7f0fe6456cfc12b3a197f7fc93f40e7d0ee550c
Manoz7 <manojkhanal936@gmail.com>
2022-12-23 10:26:09 +0545

    MBF

--- src/landingPage/banner/Banner.jsx
+++ src/landingPage/banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => (
     <div className="ap__banner-content">
       <h1 className='gradient__text'>Awiskaar Solutions </h1>
       {/* <p>Awiskaar Solutions is one of the fast emerging company in the valley. We provides every possible services related to the Technology field.</p> */}
-      <p className='types'>We Beleive in : <Typist className={'myTypist'} cursorClassName={'myCursor'} sentences={['Innovation.', 'Invention.', 'Iteration.']} loop />
+      <p className='types'>We Believe in : <Typist className={'myTypist'} cursorClassName={'myCursor'} sentences={['Innovation.', 'Invention.', 'Iteration.']} loop />
       </p>
       <div className="ap__banner-content-button">
         <button type="button"> <a href='#contact'>Get Started</a></button>
