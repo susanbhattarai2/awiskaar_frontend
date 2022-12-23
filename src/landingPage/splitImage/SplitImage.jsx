85b9f6ac09f73b54191119e218fa4243378f4462
Manoz7 <manojkhanal936@gmail.com>
2022-12-23 23:16:44 +0545

    [updated] split image

--- src/landingPage/splitImage/SplitImage.jsx
+++ src/landingPage/splitImage/SplitImage.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import './splitimage.css'
+import Typist from 'react-text-typist';
 const SplitImage = () => {
   return (
-    <div></div>
-    // <>
-    // <div className='hero'>
-    //   <div className="one">
-      
-      
-    //   </div>
-    //   <div className='three'>
-    //   <h1 className='gradient__text'>Let&apos;s Build Something Amazing with Awiskaar </h1>
-    //   <button type="button"> <a href='#contact'>Get Started</a></button>
-    //   </div>
-    //   <div className='two'>
-     
-    //   </div>
-    // </div>
-    // </>
+    // <div></div>
+    <>
+      <div className='hero'>
+        <div className="one">
+
+
+        </div>
+        <div className="ap__banner split_image_content scale-up-center" id="home">
+          <div className="ap__banner-content">
+            <h1 className='gradient__text'>Awiskaar Solutions </h1>
+            {/* <p>Awiskaar Solutions is one of the fast emerging company in the valley. We provides every possible services related to the Technology field.</p> */}
+            <p className='types'>We Believe in : <Typist className={'myTypist'} cursorClassName={'myCursor'} sentences={['Innovation.', 'Invention.', 'Iteration.']} loop />
+            </p>
+            <div className="ap__banner-content-button">
+              <button type="button"> <a href='#contact'>Get Started</a></button>
+            </div>
+          </div>
+
+         
+        </div>
+        <div className='two'>
+
+        </div>
+      </div>
+    </>
   )
 }
 
